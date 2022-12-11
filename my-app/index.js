14bf1321f2dd4929d6903e67fed078bfd7239a51
Joel Gutierrez Ruiz <joelgutierrezruiz11@gmail.com>
2022-12-10 22:15:15 -0800

    Hemos conseguido todos los canales

--- my-app/index.js
+++ my-app/index.js
@@ -1,9 +1,24 @@
 //imports
-import { Axios } from "axios";
-import express from "express";
+const axios = require("axios")
+const express = require("express")
+const cheerio = require("cheerio")
+//imports
+const url = "https://www.tvguia.es/tv/programacion-la-1"
 
 const App = express();
-Axios()
+
+axios(url,{ 
+    headers: { "Accept-Encoding": "gzip,deflate,compress" } 
+}).then(response =>{
+    const html = response.data
+    const $ = cheerio.load(html)
+    const canales = []
+    $(".block-channel-programs-div-title a", html).each(function(){
+        const canal=$(this).text()
+        canales.push(canal)
+    })
+    console.log(canales)
+})
 
 
 App.listen(8000, ()=>{console.log("Listening to port 8000")})
