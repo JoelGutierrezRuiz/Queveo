d0ec6615b5786c97a6861b7c0ee9901c4ab17165
Joel Gutierrez Ruiz <joelgutierrezruiz11@gmail.com>
2022-12-12 12:02:28 -0800

    la base esta aqui

--- my-app/index.js
+++ my-app/index.js
@@ -43,7 +43,7 @@ async function  SacarCanales()
 async function SacarProgramas(){
 
     const canales = await SacarCanales()
-    const programas ={}
+    const programas =[]
 
     canales.map(async canal=>{
         const response =await  axios(canal,{ 
@@ -57,31 +57,14 @@ async function SacarProgramas(){
         let programs = [] 
         $("#main",html).each(function(){
             channel= $(this).find("h1").text()
-            
+            programas.push(channel)
         })
-        $(".channel-programs-title a",html).each(async function(){
-            const programa = "https://www.tvguia.es"+$(this).attr("href")
-
-            const response= await axios(programa,{ 
-                headers: { "Accept-Encoding": "gzip,deflate,compress" } 
-            })
-
-            const html = response.data
-            const hola = cheerio.load(html)
-            hola(".program-title",html).each(function(){
-                const title = hola(this).text()
-                programs.push(title)
-            })
-
-
-            
-            
-        })
-       programas[channel]= programs
+       
        
 
     })
-    await sleep(10000)
+    await sleep(2000)
+    
     return programas
     
    
@@ -90,7 +73,10 @@ async function SacarProgramas(){
 
 
 
-SacarProgramas().then(response=>{console.log(response[" Programación La 1"])})
+
+
+
+SacarProgramas().then(response=>{console.log(response.length)})
 //Una vez guarda la lista de de canales vamos a buscar su programación 
 
 
