f9ec3c8b017a1566227d1a30389cd16249cde799
Joel Gutierrez Ruiz <joelgutierrezruiz11@gmail.com>
2022-12-11 20:31:29 -0800

    version funciona pero muy lento por problema del for

--- my-app/index.js
+++ my-app/index.js
@@ -1,74 +1,69 @@
 //imports
 const axios = require("axios")
 const express = require("express")
-const cheerio = require("cheerio")
+const cheerio = require("cheerio");
+const { response } = require("express");
 //imports
 const App = express();
 
 const url = "https://www.tvguia.es/tv/programacion-la-1"//url de incio
-const canales  =[]
 
-//Hacemos primera busqueda de la página en si para sacar el Html
-axios(url,{ 
-    headers: { "Accept-Encoding": "gzip,deflate,compress" } 
-})
 
-//Con el html sacado buscamos lo siguiente
-.then(response =>{
 
-    //guardamos la respuesta del callback anterior (el HTML)
-    const html = response.data
-    //Usamos cheerio para manipular el Html
-    const $ = cheerio.load(html)
-
-    //hacemos busqueda de los canales y sus links para acceder a la programación
-    $(".block-channel-programs-div-title", html).each(function(){
-        
-        //buscamos los enlaces los canales para acceder a su programación
-        const canalUrl=$(this).find("a").attr("href")
-        //guardamos en la lista la url del canal incluyendo la url principal para se valida
-        canales.push("https://www.tvguia.es"+canalUrl)
+async function  SacarCanales()
+{
+    const canales = []
+    await axios(url,{ 
+        headers: { "Accept-Encoding": "gzip,deflate,compress" } 
     })
-})
-//Una vez guarda la lista de de canales vamos a buscar su programación 
-.then(()=>{
-    //Hacemos un map de cada canal para sacar por cada canal su programación
-    titulos = 
-    canales.map(canal=>{
-        //cada enlace de canal es analizado con axios para sacar el Html
-        axios(canal,{ 
+    
+    //Con el html sacado buscamos lo siguiente
+    .then(response =>{
+    
+        //guardamos la respuesta del callback anterior (el HTML)
+        const html = response.data
+        //Usamos cheerio para manipular el Html
+        const $ = cheerio.load(html)
+    
+        //hacemos busqueda de los canales y sus links para acceder a la programación
+        $(".block-channel-programs-div-title", html).each(function(){
+            
+            //buscamos los enlaces los canales para acceder a su programación
+            const canalUrl="https://www.tvguia.es"+$(this).find("a").attr("href")
+            canales.push(canalUrl)
+        })
+    })
+    return canales
+}
+
+async function SacarProgramas(){
+    const canales = await SacarCanales()
+    const programs= []
+    
+    for(let i=0; i<canales.length;i++){
+        const response =await  axios(canales[i],{ 
             headers: { "Accept-Encoding": "gzip,deflate,compress" } 
         })
 
-        //despues de sacar el Html vamos sacar el enlace de cada programa sacado para acceder a toda su info
-        .then(response=>{
-            
-            // guardamos el html
-            const html = response.data
-            //y lo manipulamos con cheerio
-            const $ = cheerio.load(html)
-            
-            //buscamos con cheerio los programas del canal y por cada programa sacamos el enlace para acceder a toda la info
-            $(".channel-programs-title a",  html).each(function(){
-                
-                //sacamos el enlace del programa para acceder a su info completa
-                const titulo = $(this).attr("href")
-                //guardamos el enlace del programa con un enlace principal para que sea valido
-               
-                console.log( "https://www.tvguia.es"+titulo)
-                
-            }) 
-            
-            
-            
-            
-    
+
+        const html = response.data
+        const $ = cheerio.load(html)
+        $(".channel-programs-title a",html).each(function(){
+            const programa = $(this).attr("href")
+            programs.push(programa)
         })
+    }
 
-    })
+    return programs
 
-   
-})
+
+    
+    
+}
+SacarProgramas().then((response)=>{console.log(response)})
+
+//Una vez guarda la lista de de canales vamos a buscar su programación 
+
 
 
 
